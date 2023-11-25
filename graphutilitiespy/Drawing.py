93ac133bdd2b7e1d3d804bd8f2a699e7b7bf5954
Ufuk Bahçeci <98464201+UfukBahceci@users.noreply.github.com>
2023-11-25 11:41:07 +0300

    Add files via upload

--- graphutilitiespy/Drawing.py
+++ graphutilitiespy/Drawing.py
@@ -1,29 +1,32 @@
 import networkx as nx
-from scipy.sparse import csr_matrix, csgraph
 from graphutilitiespy.Graphs import *
 
-def draw_completesimplegraph(vertices, nodepos, distancetype, writeedgelabels):
+def draw_completesimplegraph(vertices, nodepos, distancetype, drawnodes, writeedgelabels):
     listofverticesandedges = create_completesimplegraph(vertices, nodepos, distancetype)
-    print('Drawing a complete simple graph..\n')
+    print('Creating a complete simple graph..\n')
     G = nx.Graph()
     G.add_nodes_from(listofverticesandedges[0])
     G.add_weighted_edges_from(listofverticesandedges[1])
-    nx.draw_networkx_nodes(G, pos = listofverticesandedges[2])
-    nx.draw_networkx_labels(G, pos = listofverticesandedges[2])
-    nx.draw_networkx_edges(G, pos = listofverticesandedges[2], edgelist=listofverticesandedges[1], arrows=True, connectionstyle='arc3, rad = 0')
-    if writeedgelabels:
-        nx.draw_networkx_edge_labels(G, pos = listofverticesandedges[2], edge_labels = nx.get_edge_attributes(G,'weight'), label_pos = 0.6, verticalalignment = 'center')    
+    if(drawnodes):
+        print('Drawing a complete simple graph..\n')
+        nx.draw_networkx_nodes(G, pos = listofverticesandedges[2])
+        nx.draw_networkx_labels(G, pos = listofverticesandedges[2])
+        nx.draw_networkx_edges(G, pos = listofverticesandedges[2], edgelist=listofverticesandedges[1], arrows=True, connectionstyle='arc3, rad = 0')
+        if writeedgelabels:
+            nx.draw_networkx_edge_labels(G, pos = listofverticesandedges[2], edge_labels = nx.get_edge_attributes(G,'weight'), label_pos = 0.6, verticalalignment = 'center')    
     return G
 
-def draw_bipartitesimplegraph(vertices, nodepos, distancetype, sizeoffirstvertexset, writeedgelabels):
+def draw_bipartitesimplegraph(vertices, nodepos, distancetype, sizeoffirstvertexset, drawnodes, writeedgelabels):
     listofverticesandedges = create_bipartitesimplegraph(vertices, nodepos, distancetype, sizeoffirstvertexset)
-    print('Drawing a bipartite simple graph..\n')
+    print('Creating a bipartite simple graph..\n')
     G = nx.Graph()
     G.add_nodes_from(listofverticesandedges[0])
     G.add_weighted_edges_from(listofverticesandedges[1])
-    nx.draw_networkx_nodes(G, pos = listofverticesandedges[2])
-    nx.draw_networkx_labels(G, pos = listofverticesandedges[2])
-    nx.draw_networkx_edges(G, pos = listofverticesandedges[2], edgelist=listofverticesandedges[1], arrows=True, connectionstyle='arc3, rad = 0')
-    if writeedgelabels:
-        nx.draw_networkx_edge_labels(G, pos = listofverticesandedges[2], edge_labels = nx.get_edge_attributes(G,'weight'), label_pos = 0.6, verticalalignment = 'center')    
+    if(drawnodes):
+        print('Drawing a bipartite simple graph..\n')
+        nx.draw_networkx_nodes(G, pos = listofverticesandedges[2])
+        nx.draw_networkx_labels(G, pos = listofverticesandedges[2])
+        nx.draw_networkx_edges(G, pos = listofverticesandedges[2], edgelist=listofverticesandedges[1], arrows=True, connectionstyle='arc3, rad = 0')
+        if writeedgelabels:
+            nx.draw_networkx_edge_labels(G, pos = listofverticesandedges[2], edge_labels = nx.get_edge_attributes(G,'weight'), label_pos = 0.6, verticalalignment = 'center')    
     return G
